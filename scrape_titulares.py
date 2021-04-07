b68560e31e400a71f9cfd6ddbb470ebbe360c41b
Pablo Sanguinetti <47663030+pcsanguinetti@users.noreply.github.com>
2021-04-07 11:21:14 +0200

    Update file location

--- scrape_titulares.py
+++ scrape_titulares.py
@@ -8,36 +8,17 @@ import random
 import csv
 import pandas as pd
 
-
-# In[3]:
-
-
 link = "https://www.elmundo.es/elecciones/elecciones-madrid.html"
 contenido = requests.get(link)
 contenido = contenido.text
 soup = BeautifulSoup(contenido, 'html.parser')
 
-
-# In[15]:
-
-
 titulares = soup.find_all("div", class_="ue-c-cover-content__main")
 
-
-# In[25]:
-
-
-print(titulares[0].a["href"].strip())
-
-
-# In[26]:
-
-
-with open('titulares_EM.csv', mode='w') as f:
+with open('data/titulares.csv', mode='w') as f:
     writer = csv.writer(f, delimiter=';')
     writer.writerow(["titular", "fecha", "diario"])
     for titular in titulares:
         t = titular.h2.text.strip()
         f = titular.a["href"]
         writer.writerow([t, f, "El Mundo"])
-
